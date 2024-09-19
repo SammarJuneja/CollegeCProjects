ffe1f0846d289874bc170e649b267285577892e6
Sammar Juneja <70101180+SammarJuneja@users.noreply.github.com>
2024-09-19 18:01:02 +0530

    Create LinerSearch.c

---
+++ LinerSearch.c
@@ -0,0 +1,29 @@
+#include<stdio.h>
+
+void main() {
+    int i;
+    int number;
+    
+    printf("Enter the number of elements: ");
+    scanf("%d", &i);
+    
+    int x[i];
+    
+    for (int y = 0; y < i; y++) {
+      printf("Enter number: ");
+      scanf("%d", &x[y]);
+    }
+    
+    printf("Enter the search query: ");
+    scanf("%d", &number);
+    
+    for (int temp = 0; temp <= i; temp++) {
+      if (x[temp] == number) {
+        printf("%d is found at %d position %d", number, temp+1);
+        break;
+      }
+      if (temp == i) {
+        printf("%d was not found", number);
+      }
+    }
+}
